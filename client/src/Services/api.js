93d13013718b06013e85ac785e8cb7a0f7d52735
Haziq Bangash <72706449+haziq-bangash@users.noreply.github.com>
2023-05-22 08:43:30 +0500

    Update api.js

--- client/src/Services/api.js
+++ client/src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://todo-app-server-eosin.vercel.app/api';
 // api request to register a new user
 export const registerUser = async (userData) => {
   try {
